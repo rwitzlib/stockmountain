ca0a88d6379eb0ae8970659a43ba061422b99f8e
Rob Witzlib <61067692+rwitzlib@users.noreply.github.com>
2026-07-04 13:44:20 -0500

    remove unneeded table

--- infra/tf/app/dynamodb.tf
+++ infra/tf/app/dynamodb.tf
@@ -80,34 +80,6 @@ resource "aws_dynamodb_table" "market_data" {
   }
 }
 
-resource "aws_dynamodb_table" "backtest" {
-  name         = "${var.team}-${var.environment}-backtest-store"
-  billing_mode = "PAY_PER_REQUEST"
-  hash_key     = "Id"
-
-  point_in_time_recovery {
-    enabled = true
-  }
-
-  deletion_protection_enabled = true
-
-  attribute {
-    name = "Id"
-    type = "S"
-  }
-
-  attribute {
-    name = "UserId"
-    type = "S"
-  }
-
-  global_secondary_index {
-    name            = "UserIndex"
-    hash_key        = "UserId"
-    projection_type = "ALL"
-  }
-}
-
 resource "aws_dynamodb_table" "user" {
   name         = "${var.team}-${var.environment}-user-store"
   billing_mode = "PAY_PER_REQUEST"
